93ae081190ca8cff8519a8dc6a2d6a75eab1799c
yukaili <yukailihz@outlook.com>
2026-07-16 17:02:03 +0800

    fix:febbox下载链接获取接口更新

--- drivers/febbox/meta.go
+++ drivers/febbox/meta.go
@@ -13,6 +13,7 @@ type Addition struct {
 	SortRule     string `json:"sort_rule" required:"true" type:"select" options:"size_asc,size_desc,name_asc,name_desc,update_asc,update_desc,ext_asc,ext_desc" default:"name_asc"`
 	PageSize     int64  `json:"page_size" required:"true" type:"number" default:"100" help:"list api per page size of FebBox driver"`
 	UserIP       string `json:"user_ip" default:"" help:"user ip address for download link which can speed up the download"`
+	CookieUI     string `json:"cookie_ui" default:"" help:"ui cookie value from browser DevTools, used for download link generation"`
 }
 
 var config = driver.Config{
